7e1457c75c97c7052b94321686fdda2ca5360b7b
Ahamathbasha <bashachristopherallah786@gmail.com>
2025-12-12 09:28:56 +0530

    finished first week of boarding

--- Revise/reactProject/src/App.jsx
+++ Revise/reactProject/src/App.jsx
@@ -11,20 +11,13 @@
 
 // export default App
 
-
-
-
-
-
-
-
-import React from 'react'
-import { BrowserRouter,Routes,Route, Link } from 'react-router-dom'
-import Home from './Router/Home'
-import Detail from './Router/Detail'
-import ContactDetail from './Router/ContactDetail'
-import ProductDetail from './Router/ProductDetail'
-import UserProfile from './Router/UserProfile'
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Router/Home";
+import Detail from "./Router/Detail";
+import ContactDetail from "./Router/ContactDetail";
+import ProductDetail from "./Router/ProductDetail";
+import UserProfile from "./Router/UserProfile";
 const App = () => {
   return (
     <BrowserRouter>
@@ -33,15 +26,15 @@ const App = () => {
       </nav>
 
       <Routes>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/detail' element={<Detail/>}>
-        <Route path='contactDetail' element={<ContactDetail/>}/>
-        <Route path='productDetail' element={<ProductDetail/>}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail" element={<Detail />}>
+          <Route path="contactDetail" element={<ContactDetail />} />
+          <Route path="productDetail" element={<ProductDetail />} />
         </Route>
-        <Route path='/user/:userId' element={<UserProfile/>}/>
+        <Route path="/user/:userId" element={<UserProfile />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
