dd3feecd3558b70102ce6f054fe0f11a1cb2b262
Ahamathbasha <bashachristopherallah786@gmail.com>
2025-12-26 12:02:00 +0530

    BOARDING WEEK CLEARED SUCCESSFULLY

---
+++ dsa/dsa3/tree.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class bst{
+    constructor(){
+        this.root = null
+    }
+
+    
+}
