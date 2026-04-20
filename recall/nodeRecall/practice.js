a915d6196fdb3ade18ae26a1fc34469274a165b5
Ahamathbasha <bashachristopherallah786@gmail.com>
2026-04-20 20:03:23 +0530

    stream revised

--- recall/nodeRecall/practice.js
+++ recall/nodeRecall/practice.js
@@ -1,6 +1,6 @@
 //write a file
 
-import fs from 'fs'
+// import fs from 'fs'
 
 // fs.writeFile('youth.txt','superbfilm',(err)=>{
 //     if(err){
@@ -29,9 +29,117 @@ import fs from 'fs'
 
 // console.log(check)
 
-fs.unlink('youthReview.txt',(err)=>{
-    if(err){
-        console.log(err)
+// fs.unlink('youthReview.txt',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log("deleted")
+// })
+
+// fs.writeFile('youth.txt','capcheck',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+
+//     console.log("writed")
+// })
+
+// fs.appendFile('youth.txt','\n do your best',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+
+//     console.log("appended")
+// })
+
+//////////////////////////////////////////////////////////
+
+// readable stream
+
+import fs from 'fs'
+
+// const readableStream = fs.createReadStream('youth.txt')
+
+// readableStream.on('data',(chunk)=>{
+//     console.log(chunk.toString())
+// })
+
+// readableStream.on('end',()=>{
+//     console.log('reading finished')
+// })
+
+// readableStream.on('error',(err)=>{
+//     console.log(err)
+// })
+
+// writable stream
+
+// const writableStream = fs.createWriteStream('ken.txt')
+
+// writableStream.write("youth is good entertaining film")
+
+// writableStream.end("\n youth")
+
+// writableStream.on('finish',()=>{
+//     console.log("writing completed")
+// })
+
+// writableStream.on('error',(err)=>{
+//     console.log('err',err)
+// })
+
+//duples stream
+
+// import { Duplex } from 'stream'
+
+// class MyDuplex extends Duplex{
+//     constructor(){
+//         super()
+//         this.data = ''
+//     }
+
+//     _write(chunk,encoding,callback){
+//         this.data +=chunk.toString() 
+//         callback()
+//     }
+
+//     _read(size){
+//         this.push(this.data)
+//         this.push(null)
+//     }
+// }
+
+// const d = new MyDuplex()
+
+// d.read('data',(chunk)=>{
+//     console.log(chunk.toString())
+// })
+
+// d.write("hello everybody")
+
+// d.read('data',(chunk)=>{
+//     console.log(chunk.toString())
+// })
+
+// d.end()
+
+// transform stream
+
+import { Transform } from 'stream'
+
+class MyTransform extends Transform{
+    _transform(chunk,encoding,callback){
+        let data = chunk.toString().toUpperCase()
+        this.push(data)
+        callback()
     }
-    console.log("deleted")
-})
+}
+
+const readable = fs.createReadStream('ken.txt')
+const writable = fs.createWriteStream('KEN2.TXT')
+
+const t = new MyTransform()
+
+
+
+readable.pipe(t).pipe(writable)
